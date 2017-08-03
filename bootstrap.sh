92b835519b5a7b93a728b05455de47919346bb45
TopConnector <constantine@mobiledatabooks.com>
2017-08-02 22:12:25 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -42,8 +42,76 @@ kubectl create -f https://rawgit.com/kubernetes/dashboard/master/src/deploy/kube
 kubectl apply -f https://raw.githubusercontent.com/topconnector/tc-kubernetes-vagrant-vmware-centos-macos/master/admin-role.yml
 
 #--- Do this manually
-#---
-#---
+---
+# grafana.yaml
+# apiVersion: extensions/v1beta1
+# kind: Deployment
+# metadata:
+#   name: monitoring-grafana
+#   namespace: kube-system
+# spec:
+#   replicas: 1
+#   template:
+#     metadata:
+#       labels:
+#         task: monitoring
+#         k8s-app: grafana
+#     spec:
+#       containers:
+#       - name: grafana
+#         image: gcr.io/google_containers/heapster-grafana-amd64:v4.4.1
+#         ports:
+#         - containerPort: 3000
+#           protocol: TCP
+#         volumeMounts:
+#         - mountPath: /var
+#           name: grafana-storage
+#         env:
+#         - name: INFLUXDB_HOST
+#           value: monitoring-influxdb
+#         - name: GF_SERVER_HTTP_PORT
+#           value: "3000"
+#           # The following env variables are required to make Grafana accessible via
+#           # the kubernetes api-server proxy. On production clusters, we recommend
+#           # removing these env variables, setup auth for grafana, and expose the grafana
+#           # service using a LoadBalancer or a public IP.
+#         - name: GF_AUTH_BASIC_ENABLED
+#           value: "false"
+#         - name: GF_AUTH_ANONYMOUS_ENABLED
+#           value: "true"
+#         - name: GF_AUTH_ANONYMOUS_ORG_ROLE
+#           value: Admin
+#         - name: GF_SERVER_ROOT_URL
+#           # If you're only using the API Server proxy, set this value instead:
+#           # value: /api/v1/proxy/namespaces/kube-system/services/monitoring-grafana/
+#           value: /
+#       volumes:
+#       - name: grafana-storage
+#         emptyDir: {}
+# ---
+# apiVersion: v1
+# kind: Service
+# metadata:
+#   labels:
+#     # For use as a Cluster add-on (https://github.com/kubernetes/kubernetes/tree/master/cluster/addons)
+#     # If you are NOT using this as an addon, you should comment out this line.
+#     kubernetes.io/cluster-service: 'true'
+#     kubernetes.io/name: monitoring-grafana
+#   name: monitoring-grafana
+#   namespace: kube-system
+# spec:
+#   # In a production setup, we recommend accessing Grafana through an external Loadbalancer
+#   # or through a public IP.
+#   # type: LoadBalancer
+#   # You could also use NodePort to expose the service at a randomly-generated port
+#   type: NodePort
+#   ports:
+#   - port: 80
+#     targetPort: 3000
+#   selector:
+#     k8s-app: grafana
+    
+#  ---   
 # vagrant ssh
 # sudo mkdir -p $HOME/.kube
 # sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
@@ -58,4 +126,12 @@ kubectl apply -f https://raw.githubusercontent.com/topconnector/tc-kubernetes-va
 # sudo cp -i ./admin.conf $HOME/.kube/config
 # sudo chown $(id -u):$(id -g) $HOME/.kube/config
 # sudo cat $HOME/.kube/config
+
+# git clone https://github.com/kubernetes/heapster.git
+# EDIT:
+
+# kubectl create -f heapster/deploy/kube-config/influxdb/
+# kubectl create -f heapster/deploy/kube-config/rbac/heapster-rbac.yaml/
+
+
 
